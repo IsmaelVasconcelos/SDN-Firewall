afaa61f7e2708ecaf12fa6e2b6251f3f389df4a3
Ismael <ismaellima807@gmail.com>
2016-10-31 16:47:41 -0300

    Funções de requisições HTTP - Comentadas

--- código.py
+++ código.py
@@ -6,7 +6,7 @@ import urllib.parse
 
 
 # Essa primeira função armazenara todas as urls que serão utilizadas para fazer as
-# requisições deste iniciar o switche a deleta as regras. Ela ira retornar un dicionário com todas a urls.
+# requisições deste iniciar o switche a deleta as regras. Ela ira retornar un dicionário com todas as urls.
 def urls ():
     url = {}
     url ["url_rules"] = 'http://localhost:8080/firewall/rules/0000000000000001'
@@ -20,6 +20,7 @@ def urls ():
     return url
 
 # Essa função ira passar os parâmetros que serão utilizados para criar as regras no firewall.
+# ela retornara um dicionário e será chamada pelas demais funções. ex: rules(), delete().
 
 def parametros_firewall ():
     newConditions = {}
@@ -30,27 +31,33 @@ def parametros_firewall ():
     newConditions ["priority"] = "10"
     return newConditions
 
+# Essa função ira passar os parâmetros para comunicação com a aplicação rest_firewalldo ryu
+# será passado o ip do servidor e a porta a qual a aplicação está trabalhando.
+
 def parametros_servidor ():
     socket = {}
     socket ["ip"]= "localhost"
     socket ["port"] = 8080
     return socket
 
+# Essa função ira ativar o firewall nos switches, será a primeira função a ser chamada.
+
 def initial_status ():
-    url_initial_status = urls ()
-    url = url_initial_status ["url_initial_status"]
-    socket = parametros_servidor ()
-    ip = socket ["ip"]
-    port = socket ["port"]
-    comment = http.client.HTTPConnection (ip, port)
-    comment.request("PUT", url)
+    url_initial_status = urls ()    # Chamando a função urls
+    url = url_initial_status ["url_initial_status"] # A url será armazenada na variável url
+    socket = parametros_servidor () # Chamando a funcão parametros_servidor
+    ip = socket ["ip"]  # O ip do servidor que está a aplicação será armazenado aqui
+    port = socket ["port"] # A porta será armazenada aqui
+    comment = http.client.HTTPConnection (ip, port)  # Essas proxímas linhas fara a requisição
+    comment.request("PUT", url)                      # utilizando o método put
     response = comment.getresponse()
 
-    status = response.status
-    reacao = response.reason
-    dados = response.read ()
+    status = response.status  # Retornara 200 se for aceita ou 404
+    reacao = response.reason  # Retornara ok se a requisição for aceita
+    dados = response.read ()  # Retornara em json o resultado o id_switche
     comment.close()
 
+# Função responsável pelas regras utilizando o método post
 def rules ():
     url_rules = urls ()
     newConditions = parametros_firewall ()
@@ -60,8 +67,10 @@ def rules ():
                                 headers={'content-type': 'application/json'})
     response = urllib.request.urlopen(req)
 
-    saida = response.read().decode('utf8')
+    saida = response.read().decode('utf8') # Retornara em json o resultado o id_switche
 
+
+# Função responsável por retornar o status do switche se está ativo ou não e sua id
 def status_switche ():
     url_status_switche = urls ()
     url = url_status_switche ["url_status_switche"]
@@ -76,6 +85,7 @@ def status_switche ():
     reacao = response.reason
     dados = response.read ()
 
+# Função responsável por deleta as regras enviadas, OBS: Não está funcionando
 def delete ():
     body = {"rule_id": "5"}
     conn = httplib.HTTPConnection('localhost', 8080)
@@ -83,6 +93,8 @@ def delete ():
     resp = conn.getresponse()
     content = resp.read()
 
+
+# Função que ira mostrar todas as regras estabelecidas o switche
 def acquiring_all_rules ():
     all_rules = urls ()
     url = all_rules ["url_all_rules"]
@@ -97,6 +109,8 @@ def acquiring_all_rules ():
     status = response.status
     reacao = response.reason
 
+
+# Função para adquirir os logs de saída
 def acquiring_log_output ():
     acquiring_log = urls()
     url = acquiring_log ["url_acquiring_log"]
